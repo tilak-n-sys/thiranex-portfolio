af307cb20379ef873210be8d14889a22c036e595
Tilak N <tilakn134@gmail.com>
2026-08-16 20:59:23 +0530

    Refactor theme toggle button event listener

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const button = document.getElementById("theme-toggle");
 
-if (button) {
-  button.addEventListener("click", function () {
+button.addEventListener("click", function () {
     document.body.classList.toggle("light-mode");
-  });
-}
+});
